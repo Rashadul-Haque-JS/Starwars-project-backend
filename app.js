f6f7dadefe2ef6283ad9b119de32549a38b3cabf
rashadul-haque-js <mohammed.haque@iths.se>
2022-03-23 09:09:17 +0100

    updated with new files

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
 const express = require('express')
-const { listen } = require('express/lib/application')
 const app = express()
-
+app.use(express.static('public'))
+app.set('view engine', 'ejs');
 const { PromisedDatabase } = require("promised-sqlite3")
-
 const db = new PromisedDatabase()
-async function test() {
-    try {
-        await db.open("./chinook.db"); 
-  
-        const row = await db.get("SELECT * FROM customers");
-        console.log(row);
-        await db.close();
 
-    } catch(err) {
+// Create table with values
+async function InsertChar() {
+    try {
+
+        await db.open("./starwars.db");
+        await db.run("CREATE TABLE IF NOT EXISTS starfighters( FirstName TEXT  NOT NULL,LastName TEXT  NOT NULL,Planet TEXT NOT NULL,Galaxy TEXT NOT NULL, Email TEXT NOT NULL UNIQUE)")
+
+        // await db.run("INSERT INTO starfighters ('FirstName','LastName','Planet','Galaxy','Email' ) VALUES ('Luke', 'Skywalker', 'Tatooine', 'GFFA', 'luke.skywalker@galaxy.com')");
+
+        // await db.run("INSERT INTO starfighters ('FirstName','LastName','Planet','Galaxy','Email' ) VALUES ('Leia ', 'Organa', 'Alderaan','GFFA', 'leia.organa@galaxy.com')");
+
+        // await db.run("INSERT INTO starfighters ('FirstName','LastName','Planet','Galaxy','Email' ) VALUES ('Lando', 'Calrissian', 'Socorro','GFFA', 'lando.carlissian@galaxy.com')");
+
+        // await db.run("INSERT INTO starfighters ('FirstName','LastName','Planet','Galaxy','Email' ) VALUES ('Darth ', 'Maul', 'Dathomir','GFFA', 'darth.maul@galaxy.com')");
+
+      } catch (err) {
         console.error(err);
     }
 }
 
-test();
+InsertChar()
+
+// Fetch single value (by command 'db.get') or all values( by command 'db.all') from the created table
+async function getStarFighters() {
+    try {
+        await db.open("./starwars.db");
+        // const row = await db.all("SELECT * FROM starfighters");
+        // const row = await db.get("SELECT * FROM starfighters WHERE Galaxy IS 'GFFA'");
+        const row = await db.all("SELECT * FROM starfighters WHERE Galaxy IS 'GFFA'");
+
+        await db.close();
+        return row
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Delete table 
+
+/*
+async function removeTable() {
+   try {
+   await db.open("./starwars.db")
+   await db.run("DROP TABLE starfighters")
+   } catch (err) {
+       console.log(err)
+   }
+}
+removeTable() 
+*/
+
+app.get('/', async (req, res) => {
+    res.redirect('pages/character')
+
+})
+
+app.get('/pages/character', async (req, res) => {
+    let starfighters = await getStarFighters()
+    res.render('pages/character', { starfighters })
+})
 
 
 
-listen(8000)
-console.log('server on port 8000')
+app.listen(8000)
+console.log('server running on port 8000')
