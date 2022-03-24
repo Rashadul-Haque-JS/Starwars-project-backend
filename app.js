91ef42034e41ce767cd2438aa58b89fcd2c76b09
rashadul-haque-js <mohammed.haque@iths.se>
2022-03-24 18:37:03 +0100

    Updated

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
+const axios = require('axios');
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 const { PromisedDatabase } = require("promised-sqlite3")
 const db = new PromisedDatabase()
+const getChar = require('./modules/char')
+
+let characters;
+async function getResponse() {
+
+characters = await getChar.fetchChar(axios);
+console.log(characters)
+} 
+
+getResponse()
+
+
 
 // Create table with values
 async function InsertChar() {
@@ -20,12 +33,15 @@ async function InsertChar() {
 
         // await db.run("INSERT INTO starfighters ('FirstName','LastName','Planet','Galaxy','Email' ) VALUES ('Darth ', 'Maul', 'Dathomir','GFFA', 'darth.maul@galaxy.com')");
 
+
       } catch (err) {
         console.error(err);
     }
 }
 
-InsertChar()
+InsertChar() 
+
+
 
 // Fetch single value (by command 'db.get') or all values( by command 'db.all') from the created table
 async function getStarFighters() {
@@ -57,10 +73,21 @@ async function removeTable() {
 removeTable() 
 */
 
-app.get('/', async (req, res) => {
-    res.redirect('pages/character')
+let charcs;
 
+app.get('/', (req, res) => {
+res.render('pages/charDetails', {characters})
+  
 })
+
+
+app.post('/charsCard', (req, res) => {
+    charcs = req.body
+    console.log(charcs)
+    res.redirect('pages/character')
+})
+
+
 
 app.get('/pages/character', async (req, res) => {
     let starfighters = await getStarFighters()
@@ -68,6 +95,5 @@ app.get('/pages/character', async (req, res) => {
 })
 
 
-
-app.listen(8000)
-console.log('server running on port 8000')
+app.listen(5000)
+console.log('server running on port 5000')
